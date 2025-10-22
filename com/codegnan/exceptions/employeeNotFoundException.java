8694981b1defcef67daf99676ad88edafa786749
Vamsi Sandu <sandu.vamsi@gmail.com>
2025-10-22 10:36:09 +0530

    6

--- com/codegnan/exceptions/employeeNotFoundException.java
+++ com/codegnan/exceptions/employeeNotFoundException.java
@@ -1,15 +1,5 @@
 package com.codegnan.exceptions;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.*;
-
-import com.codegnan.dao.employeeDao;
-import com.codegnan.model.employee;
-import com.codegnan.util.dbConnectionUtil;
-
 public class employeeNotFoundException extends Exception {
     public employeeNotFoundException(String msg) {
         super(msg);
